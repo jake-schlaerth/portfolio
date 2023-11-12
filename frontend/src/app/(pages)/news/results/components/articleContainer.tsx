5ae7bfcec40c9f6f00241cf2acb8a47cf52899f2
jake-schlaerth <jakeschlaerth1999@gmail.com>
2023-11-12 11:32:36 -0500

    remove news

--- frontend/src/app/(pages)/news/results/components/articleContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface ArticleContainerProps {
-  children: ReactNode;
-}
-
-export const ArticleContainer = ({ children }: ArticleContainerProps) => (
-  <div className="flex justify-center space-x-4">{children}</div>
-);
