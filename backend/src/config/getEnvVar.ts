ca095b7e8ae6d9461f5a8f6e58aeb3d9bba3c0ca
jake <jake@JACOBSCHLAERTH-MAC.attlocal.net>
2024-01-24 09:45:11 -0500

    remove backend code

--- backend/src/config/getEnvVar.ts
+++
@@ -1,9 +0,0 @@
-export const getEnvVar = (key: string): string => {
-  const value = process.env[key];
-
-  if (typeof value === "undefined") {
-    throw new Error(`Environment variable ${key} is not set`);
-  }
-
-  return value;
-};
--- backend/src/routes/newsRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { newsController } from "@/controllers/newsController/newsController";
-
-const router = express.Router();
-
-router.get("/news", newsController.getNews);
-
-export default router;
