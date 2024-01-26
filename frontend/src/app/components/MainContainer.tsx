5adc29388ee8ea2736cd7c4db67dc2fa29faf53b
jake <jakeschlaerth1999@gmail.com>
2024-01-26 10:29:27 -0500

    simplify main container component

--- frontend/src/app/components/MainContainer.tsx
+++ frontend/src/app/components/MainContainer.tsx
@@ -1,10 +1,4 @@
-import { FC, ReactNode } from "react";
-
-interface MainContainerProps {
-  children?: ReactNode;
-}
-
-export const MainContainer: FC<MainContainerProps> = ({ children }) => (
+export const MainContainer = ({ children }: {children: React.ReactNode}) => (
   <div className="min-h-screen flex items-center justify-center flex-col">
     {children}
   </div>
