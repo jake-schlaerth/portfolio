fa7e255e7c53978571ba76efdcd34b69dd409a65
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-02-13 19:41:55 -0500

    add health check to analytics streamer

--- analytics-streamer/src/http/server.ts
+++ analytics-streamer/src/http/server.ts
@@ -2,7 +2,16 @@ import http from "http";
 import { config } from "@/config";
 
 const setupServer = () => {
-  const server = http.createServer();
+  const server = http.createServer((req, res) => {
+    if (req.url === "/healthz") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ status: "up" }));
+    } else {
+      res.statusCode = 404;
+      res.end("Not Found");
+    }
+  });
 
   server.listen(config.port, () => {
     console.log(`Server started on port ${config.port}`);
