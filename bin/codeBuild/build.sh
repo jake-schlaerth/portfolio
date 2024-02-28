b49d9f8466a5236db4a72e847d61d726e98b633f
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-02-28 17:12:50 -0500

    add quotes for interpolated strings in build.sh

--- bin/codeBuild/build.sh
+++ bin/codeBuild/build.sh
@@ -7,7 +7,7 @@ fi
 
 APP_NAME=$1
 CURRENT_HASH=$(turbo run build --dry=json --filter $APP_NAME | jq ".tasks[] | select(.taskId == \"$APP_NAME#build\").hash" -r)
-PREVIOUS_HASH=$(aws s3 cp s3://turbo-hashes/$APP_NAME-hash.txt -)
+PREVIOUS_HASH=$(aws s3 cp "s3://turbo-hashes/$APP_NAME-hash.txt" -)
 
 pre_build() {
   echo "Starting pre-build steps for $APP_NAME..."
@@ -17,10 +17,10 @@ pre_build() {
     CODEPIPELINE_PIPELINE_NAME="${APP_NAME}-pipeline"
 
     aws codepipeline list-pipeline-executions \
-      --pipeline-name $CODEPIPELINE_PIPELINE_NAME | \
+      --pipeline-name "$CODEPIPELINE_PIPELINE_NAME" | \
     jq -r '.pipelineExecutionSummaries[] | select(.status == "InProgress") | .pipelineExecutionId' | \
     xargs -I {} aws codepipeline stop-pipeline-execution \
-      --pipeline-name $CODEPIPELINE_PIPELINE_NAME \
+      --pipeline-name "$CODEPIPELINE_PIPELINE_NAME" \
       --pipeline-execution-id {} \
       --abandon \
       --reason "Cache hit for $APP_NAME, aborting build"
@@ -33,13 +33,13 @@ pre_build() {
 build() {
   echo "Building $APP_NAME..."
   echo Logging in to Docker Hub...
-  docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+  docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
   echo Building the Docker image...
-  docker build -t $REPOSITORY_URI:latest ./apps/$APP_NAME -f Dockerfile.production
+  docker build -t "$REPOSITORY_URI:latest" -f "./apps/$APP_NAME/Dockerfile.production" .
   echo Logging in to Amazon ECR...
-  $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
+  $(aws ecr get-login --region "$AWS_DEFAULT_REGION" --no-include-email)
   echo Pushing the Docker image...
-  docker push $REPOSITORY_URI:latest
+  docker push "$REPOSITORY_URI:latest"
   echo Writing image definitions file...
   printf '[{"name":"%s-image","imageUri":"%s"}]' "$APP_NAME" "$REPOSITORY_URI:latest" > imagedefinitions.json
 
@@ -48,8 +48,8 @@ build() {
 
 post_build() {
   echo "Post-build steps for $APP_NAME..."
-  echo $CURRENT_HASH > $APP_NAME-build-hash.txt
-  aws s3 cp $APP_NAME-build-hash.txt s3://turbo-hashes/$APP_NAME-hash.txt
+  echo "$CURRENT_HASH" > "$APP_NAME-build-hash.txt"
+  aws s3 cp "$APP_NAME-build-hash.txt" "s3://turbo-hashes/$APP_NAME-hash.txt"
   echo "Post-build steps completed."
 }
 
