e4d3f217869d7c2ef6c55936f831d5589532c45f
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-02-28 13:00:01 -0500

    dockerfile adjustments to generate .env at runtime instead of baking into the image

---
+++ bin/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SECRET_NAME="${SECRET_NAME}"
+ENV_FILE_PATH="${ENV_FILE_PATH}"
+
+if [ -z "$SECRET_NAME" ] || [ -z "$ENV_FILE_PATH" ]; then
+    echo "Environment variables SECRET_NAME and ENV_FILE_PATH must be set."
+    exit 1
+fi
+
+./bin/build-env/build-env.sh "$SECRET_NAME" "$ENV_FILE_PATH"
+
+node $1
