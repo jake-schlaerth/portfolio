2c300c97a4a99bf6f778c72e9b532e9d364c4059
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-02-29 19:23:44 -0500

    add sasl creds to kafka client

--- apps/analytics-streamer/src/kafka/kafkaClient.ts
+++ apps/analytics-streamer/src/kafka/kafkaClient.ts
@@ -1,8 +1,15 @@
-import { Kafka } from "kafkajs";
+import { Kafka, logLevel } from "kafkajs";
 
 import { getEnvVar } from "utils";
 
 export const kafkaClient = new Kafka({
   clientId: getEnvVar("KAFKA_CLIENT_ID"),
   brokers: [getEnvVar("KAFKA_BROKER_BASE_URL")],
+  ssl: true,
+  sasl: {
+    mechanism: "scram-sha-256",
+    username: getEnvVar("KAFKA_USERNAME"),
+    password: getEnvVar("KAFKA_PASSWORD"),
+  },
+  logLevel: logLevel.ERROR,
 });
