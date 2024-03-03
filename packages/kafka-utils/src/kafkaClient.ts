981352dea27c8d4be51852383ff0f0b7d6435ef5
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-03-02 21:11:41 -0500

    add env var to denote prod from dev

--- packages/kafka-utils/src/kafkaClient.ts
+++ packages/kafka-utils/src/kafkaClient.ts
@@ -5,6 +5,7 @@ import { getEnvVar } from "utils";
 export const kafkaClient = new Kafka({
   clientId: getEnvVar("KAFKA_CLIENT_ID"),
   brokers: [getEnvVar("KAFKA_BROKER_BASE_URL")],
+  ssl: getEnvVar("ENV") === "prod",
   sasl: {
     mechanism: "scram-sha-256",
     username: getEnvVar("KAFKA_USERNAME"),
