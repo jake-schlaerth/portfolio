8ffc36c4e73ff41cf86587c131825e3e2a8d8278
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-03-04 16:34:44 -0500

    add env vars to buildspec and as build args

--- apps/frontend/bin/build.sh
+++ apps/frontend/bin/build.sh
@@ -39,7 +39,24 @@ build() {
   echo Logging in to Docker Hub...
   docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
   echo Building the Docker image...
-  docker build --build-arg DB_USER="${DB_USER}" -t "$REPOSITORY_URI:latest" -f "./apps/$APP_NAME/Dockerfile.production" .
+ docker build \
+  --build-arg DB_USER="${DB_USER}" \
+  --build-arg ENV="${ENV}" \
+  --build-arg KAFKA_BROKER_BASE_URL="${KAFKA_BROKER_BASE_URL}" \
+  --build-arg KAFKA_USERNAME="${KAFKA_USERNAME}" \
+  --build-arg KAFKA_PASSWORD="${KAFKA_PASSWORD}" \
+  --build-arg KAFKA_TOPIC="${KAFKA_TOPIC}" \
+  --build-arg KAFKA_CLIENT_ID="${KAFKA_CLIENT_ID}" \
+  --build-arg KAFKA_GROUP_ID="${KAFKA_GROUP_ID}" \
+  --build-arg DB_NAME="${DB_NAME}" \
+  --build-arg DB_URI="${DB_URI}" \
+  --build-arg DB_PASSWORD="${DB_PASSWORD}" \
+  --build-arg REDIS_BASE_URL="${REDIS_BASE_URL}" \
+  --build-arg DB_SSL_MODE="${DB_SSL_MODE}" \
+  --build-arg WEBSOCKET_SERVER="${WEBSOCKET_SERVER}" \
+  -t "$REPOSITORY_URI:latest" \
+  -f "./apps/$APP_NAME/Dockerfile.production" .
+
   echo Logging in to Amazon ECR...
   aws ecr get-login-password --region "$AWS_DEFAULT_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
   echo Pushing the Docker image...
