f728fa7e2405a20a9c8c1b06d1b5e301c57e04ca
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-03-04 19:49:47 -0500

    comment out redis caching mechanism for now

--- apps/frontend/src/app/(api)/locations/route.ts
+++ apps/frontend/src/app/(api)/locations/route.ts
@@ -4,20 +4,20 @@ import { getLocationsFromDatabase } from "./getLocationsFromDatabase";
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  const redisClient = await setupRedisClient();
-  const cacheKey = "locations";
-  let locations;
+  // const redisClient = await setupRedisClient();
+  // const cacheKey = "locations";
+  // let locations;
 
-  const cachedLocations = await redisClient.get(cacheKey);
-  if (cachedLocations) {
-    locations = JSON.parse(cachedLocations);
-  } else {
-    locations = await getLocationsFromDatabase();
+  // const cachedLocations = await redisClient.get(cacheKey);
+  // if (cachedLocations) {
+  //   locations = JSON.parse(cachedLocations);
+  // } else {
+  //   locations = await getLocationsFromDatabase();
 
-    await redisClient.set(cacheKey, JSON.stringify(locations), {
-      EX: 60,
-    });
-  }
+  //   await redisClient.set(cacheKey, JSON.stringify(locations), {
+  //     EX: 60,
+  //   });
+  // }
 
-  return new Response(JSON.stringify(locations));
+  return new Response(JSON.stringify(await getLocationsFromDatabase()));
 }
