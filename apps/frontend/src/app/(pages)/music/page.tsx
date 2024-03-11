ceb4924a93450803704fc506fd9e997201886ac9
jake-schlaerth <jakeschlaerth1999@gmail.com>
2024-03-11 16:07:05 -0400

    remove music page, replace with both page

--- apps/frontend/src/app/(pages)/music/page.tsx
+++ apps/frontend/src/app/(pages)/music/page.tsx
@@ -1,9 +1,25 @@
-const Music = () => (
-  <iframe
-    style={{ border: "0", width: "350px", height: "470px" }}
-    src="https://bandcamp.com/EmbeddedPlayer/album=305360011/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
-    seamless
-  />
+"use client";
+
+import { ChordGenerator, StartModal } from "./components";
+import { Canvas } from "../../components/Canvas";
+import { C_MAJOR_7, F_MINOR_7 } from "./consts/chords";
+
+const Both = () => (
+  <>
+    <StartModal />
+    <div className="flex flex-row space-x-16">
+      <ChordGenerator midiNotes={C_MAJOR_7} />
+      <ChordGenerator midiNotes={F_MINOR_7} />
+    </div>
+    <Canvas
+      canvasHeight={600}
+      canvasWidth={800}
+      displayHeight={600}
+      displayWidth={800}
+      frequencyX={0.2}
+      frequencyY={0.191}
+    />
+  </>
 );
 
-export default Music;
+export default Both;
