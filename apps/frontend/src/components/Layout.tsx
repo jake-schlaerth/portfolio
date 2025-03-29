e096423059ac3b2648d2bdcb56bd4dcf25730bf5
jake-schlaerth <jakeschlaerth1999@gmail.com>
2025-03-29 18:50:38 -0400

    update layout

--- apps/frontend/src/components/Layout.tsx
+++ apps/frontend/src/components/Layout.tsx
@@ -9,9 +9,7 @@ export const Layout = ({ children }: PageProps) => {
   return (
     <div className="min-h-screen min-w-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 flex flex-col items-center justify-center">
-        {children}
-      </main>
+      <main className="flex-1 flex flex-col items-center">{children}</main>
     </div>
   );
 };
