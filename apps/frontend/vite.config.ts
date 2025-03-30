e5324d044149f53e34a69ec6cfbb27648b934c14
jake-schlaerth <jakeschlaerth1999@gmail.com>
2025-03-29 20:28:12 -0400

    add env.prod for frontend to point to the right backend url

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    // Expose ENV variables to the client
+    define: {
+      "process.env.VITE_BACKEND_BASE_URL": JSON.stringify(
+        env.VITE_BACKEND_BASE_URL
+      ),
+    },
+  };
 });
