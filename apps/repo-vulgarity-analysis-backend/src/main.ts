ab0dc1ef28dc0dbc0a8bc1f743c5c3ce9eb57688
jake-schlaerth <jakeschlaerth1999@gmail.com>
2025-04-13 20:53:46 -0400

    cors

--- apps/repo-vulgarity-analysis-backend/src/main.ts
+++ apps/repo-vulgarity-analysis-backend/src/main.ts
@@ -1,20 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const config = new DocumentBuilder()
-    .setTitle('Vulgarity Analysis API')
-    .setDescription('API for analyzing text vulgarity')
-    .setVersion('1.0')
-    .build();
-    
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle("Vulgarity Analysis API")
+    .setDescription("API for analyzing text vulgarity")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.enableCors({
+    origin: ["https://jakeschlaerth.com", "http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
-bootstrap(); 
+bootstrap();
