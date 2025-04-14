a929f808e44450993d322a52ce47c22621db86c5
jake-schlaerth <jakeschlaerth1999@gmail.com>
2025-04-13 22:41:10 -0400

    implicit return

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,9 +1,5 @@
-export const Header = () => {
-  return (
-    <>
-      <h1 className="text-6xl font-thin leading-tight my-8">
-        repo vulgarity analysis
-      </h1>
-    </>
-  );
-};
+export const Header = () => (
+  <h1 className="text-6xl font-thin leading-tight my-8">
+    repo vulgarity analysis
+  </h1>
+);
